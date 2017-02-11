ca1b12ec1044991da4500c6959b9546b3ef0d72e
joshkestenberg <joshkestenberg@gmail.com>
2017-02-11 17:11:17 -0500

    added responses for html csv plain and json

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,30 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
+
+    respond_to do |format|
+      format.html
+      format.text
+      format.csv do
+        render plain: Book.generate_csv(@books)
+      end
+      format.json do
+        render json: @books
+      end
+    end
+  end
+
+  def self.generate_csv(book_list)
+  header = ['id', 'title', 'author', 'already_read']
+
+  CSV.generate(headers: true) do |csv|
+    csv << header
+
+    book_list.each do |book|
+      csv << [book.id, book.title, book.author, book.already_read]
+    end
+  end
+end
 
 end
